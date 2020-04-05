54680b83ba3f02ebc4e25cc841f5382ab0730aad
PetteriM1 <petteri.malkavaara1@gmail.com>
2020-04-05 23:17:33 +0300

    Initial commit

---
+++ src/main/java/me/petterim1/optimizeddroppeditems/Main.java
@@ -0,0 +1,12 @@
+package me.petterim1.optimizeddroppeditems;
+
+import cn.nukkit.entity.Entity;
+import cn.nukkit.plugin.PluginBase;
+
+public class Main extends PluginBase {
+
+    public void onEnable() {
+        Entity.registerEntity("Item", OptimizedDroppedItem.class);
+        getLogger().info("Optimized dropped item entity registered");
+    }
+}
---
+++ src/main/java/me/petterim1/optimizeddroppeditems/OptimizedDroppedItem.java
@@ -0,0 +1,124 @@
+package me.petterim1.optimizeddroppeditems;
+
+import cn.nukkit.Player;
+import cn.nukkit.Server;
+import cn.nukkit.entity.Entity;
+import cn.nukkit.entity.item.EntityItem;
+import cn.nukkit.item.Item;
+import cn.nukkit.level.format.FullChunk;
+import cn.nukkit.nbt.tag.CompoundTag;
+import cn.nukkit.network.protocol.EntityEventPacket;
+
+public class OptimizedDroppedItem extends EntityItem {
+
+    public OptimizedDroppedItem(FullChunk chunk, CompoundTag nbt) {
+        super(chunk, nbt);
+    }
+
+    @Override
+    public String getName() {
+        return "OptimizedDroppedItem";
+    }
+
+    @Override
+    public boolean onUpdate(int currentTick) {
+        if (this.closed) {
+            return false;
+        }
+
+        int tickDiff = currentTick - this.lastUpdate;
+
+        if (tickDiff <= 0 && !this.justCreated) {
+            return true;
+        }
+
+        this.lastUpdate = currentTick;
+
+        this.timing.startTiming();
+
+        if (this.age > 4800 || this.isInsideOfFire()) {
+            this.close();
+            this.timing.stopTiming();
+            return false;
+        }
+
+        if (this.age % 200 == 0 && this.onGround && this.item != null && this.isAlive()) {
+            if (this.item.getCount() < this.item.getMaxStackSize()) {
+                Entity[] e = this.getLevel().getNearbyEntities(getBoundingBox().grow(1, 1, 1), this, false);
+                for (Entity entity : e) {
+                    if (entity instanceof EntityItem) {
+                        if (!entity.isAlive()) {
+                            continue;
+                        }
+                        Item closeItem = ((EntityItem) entity).getItem();
+                        if (!closeItem.equals(item, true, true)) {
+                            continue;
+                        }
+                        if (!entity.isOnGround()) {
+                            continue;
+                        }
+                        int newAmount = this.item.getCount() + closeItem.getCount();
+                        if (newAmount > this.item.getMaxStackSize()) {
+                            continue;
+                        }
+                        closeItem.setCount(0);
+                        entity.close();
+                        this.item.setCount(newAmount);
+                        EntityEventPacket packet = new EntityEventPacket();
+                        packet.eid = getId();
+                        packet.data = newAmount;
+                        packet.event = EntityEventPacket.MERGE_ITEMS;
+                        Server.broadcastPacket(this.getLevel().getPlayers().values(), packet);
+                    }
+                }
+            }
+        }
+
+        boolean hasUpdate = this.entityBaseTick(tickDiff);
+
+        if (this.isAlive()) {
+            if (this.pickupDelay > 0 && this.pickupDelay < 32767) {
+                this.pickupDelay -= tickDiff;
+                if (this.pickupDelay < 0) {
+                    this.pickupDelay = 0;
+                }
+            } else {
+                for (Entity entity : this.level.getNearbyEntities(this.boundingBox.grow(1, 0.5, 1), this)) {
+                    if (entity instanceof Player) {
+                        if (((Player) entity).pickupEntity(this, true)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            if (this.isInsideOfWater()) {
+                this.motionY = -0.02;
+            } else if (!this.isOnGround()) {
+                this.motionY -= 0.04;
+            }
+
+            this.move(this.motionX, this.motionY, this.motionZ);
+
+            double friction = 1 - this.getDrag();
+
+            if (this.onGround && (Math.abs(this.motionX) > 0.00001 || Math.abs(this.motionZ) > 0.00001)) {
+                friction *= this.getLevel().getBlock(this.temporalVector.setComponents((int) Math.floor(this.x), (int) Math.floor(this.y - 1), (int) Math.floor(this.z) - 1)).getFrictionFactor();
+            }
+
+            this.motionX *= friction;
+            this.motionY *= 1 - this.getDrag();
+            this.motionZ *= friction;
+
+            if (this.onGround) {
+                this.motionY *= -0.5;
+            }
+
+            this.updateMovement();
+        }
+
+        this.timing.stopTiming();
+
+        return hasUpdate || !this.onGround || Math.abs(this.motionX) > 0.00001 || Math.abs(this.motionY) > 0.00001 || Math.abs(this.motionZ) > 0.00001;
+    }
+}
