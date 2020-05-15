4d7c3f22b49b612d3bfd0284ffee29ee6ae7c035
Petteri <petteri.malkavaara1@gmail.com>
2020-05-15 11:49:43 +0300

    Stuff

--- src/main/java/me/petterim1/optimizeddroppeditems/OptimizedDroppedItem.java
+++ src/main/java/me/petterim1/optimizeddroppeditems/OptimizedDroppedItem.java
@@ -98,15 +98,9 @@ public class OptimizedDroppedItem extends EntityItem {
 
             this.move(this.motionX, this.motionY, this.motionZ);
 
-            double friction = 0.98;
-
-            if (this.onGround && (Math.abs(this.motionX) > 0.00001 || Math.abs(this.motionZ) > 0.00001)) {
-                friction *= this.getLevel().getBlock(this.temporalVector.setComponents((int) Math.floor(this.x), (int) Math.floor(this.y - 1), (int) Math.floor(this.z) - 1)).getFrictionFactor();
-            }
-
-            this.motionX *= friction;
+            this.motionX *= 0.9;
             this.motionY *= 0.98;
-            this.motionZ *= friction;
+            this.motionZ *= 0.9;
 
             if (this.onGround) {
                 this.motionY *= -0.5;
